5053b391105ae260ab996037986b6a8bd1cb75d6
Jackson Tong <tongtoan2704@gmail.com>
2022-04-23 21:01:29 +0700

    install graphql

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,36 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import dotenv from "dotenv";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+// middlewares
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server");
